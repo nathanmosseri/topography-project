e1d081a3c604d5efb5be8d3cef5688e767c03bb6
Nathan <namo678@me.com>
2022-09-12 11:26:17 -0400

    nothing yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import DeckGL from '@deck.gl/react';
+import {GeoJsonLayer} from '@deck.gl/layers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({data, viewState}) {
+  /**
+   * Data format:
+   * Valid GeoJSON object
+   */
+  const layer = new GeoJsonLayer({
+    id: 'geojson-layer',
+    data,
+    pickable: true,
+    stroked: false,
+    filled: true,
+    extruded: true,
+    pointType: 'circle',
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [0, 160, 180, 200],
+    // getLineColor: d => colorToRGBArray(d.properties.color),
+    getPointRadius: 100,
+    getLineWidth: 1,
+    getElevation: 30
+  });
+
+  return <DeckGL viewState={viewState}
+    layers={[layer]}
+    getTooltip={({object}) => object && (object.properties.name || object.properties.station)} />;
 }
 
 export default App;
